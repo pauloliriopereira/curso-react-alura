92686c4187884b9f506d9010846761f9b6ad621e
paulo.lirio <paulo.lirio@impa.br>
2020-07-14 09:05:01 -0300

    Melhoria no projeto curso alura parte 3

--- src/Components/Tabela/Tabela.js
+++ src/Components/Tabela/Tabela.js
@@ -1,38 +1,10 @@
 import React, { Component } from 'react';
-
-const TableHead = () => {
-    return (
-        <thead>
-            <tr>
-                <th>Autores</th>
-                <th>Livros</th>
-                <th>Preços</th>
-                <th>Remover</th>
-            </tr>
-        </thead>
-    );
-}
-
-
-const TableBody = props => {
-    const linhas = props.autores.map((linha) => {
-        return (
-            <tr key = {linha.id}>
-                <td>{linha.nome}</td>
-                <td>{linha.livro}</td>
-                <td>{linha.preco}</td>
-                <td>
-                    <button className="waves-effect waves-light btn indigo lighten-2" onClick = { () => { props.removeAutor(linha.id) }}>Remover</button>
-                </td>
-            </tr>
-        );
-    });
-    return (
-        <tbody>
-           {linhas}
-        </tbody>
-    );
-}
+import Table from '@material-ui/core/Table';
+import TableBody from '@material-ui/core/TableBody';
+import TableCell from '@material-ui/core/TableCell';
+import TableHead from '@material-ui/core/TableHead';
+import TableRow from '@material-ui/core/TableRow';
+import Button from '@material-ui/core/Button'
 
 class Tabela extends Component {
 
@@ -40,10 +12,30 @@ class Tabela extends Component {
         const { autores, removeAutor } = this.props;
 
         return (
-            <table className="centered highlight">
-                <TableHead />
-                <TableBody autores = { autores } removeAutor = {removeAutor} />
-            </table>
+            <Table>
+                 <TableHead>
+                    <TableRow>
+                        <TableCell>Autores</TableCell>
+                        <TableCell>Livros</TableCell>
+                        <TableCell>Preços</TableCell>
+                        <TableCell>Remover</TableCell>
+                    </TableRow>
+                </TableHead>
+                <TableBody>
+                    {
+                        autores.map(autor => (
+                            <TableRow key = {autor.id}>
+                                <TableCell>{autor.nome}</TableCell>
+                                <TableCell>{autor.livro}</TableCell>
+                                <TableCell>{autor.preco}</TableCell>
+                                <TableCell>
+                                    <Button variant="contained" color="primary" onClick = { () => { removeAutor(autor.id) }}>Remover</Button>
+                                </TableCell>
+                            </TableRow>
+                        ))
+                    }
+                </TableBody>
+            </Table>
         );
     }
 
--- src/index.js
+++ src/index.js
@@ -8,7 +8,6 @@ import Livros from './Pages/Livros/Livros';
 import NotFound from './Pages/NotFound/NotFound';
 
 import { BrowserRouter, Switch, Route } from 'react-router-dom';
-import * as serviceWorker from './serviceWorker';
 
 ReactDOM.render(
   <BrowserRouter>
@@ -22,8 +21,3 @@ ReactDOM.render(
   </BrowserRouter>,
   document.getElementById('root')
 );
-
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
